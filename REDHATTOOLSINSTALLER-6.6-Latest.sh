14ff40964ca64193c80acf317430c9b66e76f439
sgallego <sgallego@taco.test.spg>
2019-12-05 13:43:16 -0700

    as

--- REDHATTOOLSINSTALLER-6.6-Latest.sh
+++ REDHATTOOLSINSTALLER-6.6-Latest.sh
@@ -771,10 +771,10 @@ satellite-installer --scenario satellite -v \
 --foreman-cli-username=$ADMIN \
 --foreman-cli-password=$ADMIN_PASSWORD \
 --foreman-initial-admin-username=$ADMIN \
---foreman-initial-admin-password=$ADMIN_PASSWORD
+--foreman-initial-admin-password=$ADMIN_PASSWORD \
 --foreman-proxy-plugin-remote-execution-ssh-install-key true \
 --foreman-initial-organization=$ORG \
---foreman-initial-location=$LOC
+--foreman-initial-location=$LOC \
 --foreman-proxy-dns true \
 --foreman-proxy-dns-managed=true \
 --foreman-proxy-dns-provider=nsupdate \
@@ -783,7 +783,7 @@ satellite-installer --scenario satellite -v \
 --foreman-proxy-dns-zone=$DOM \
 --foreman-proxy-dns-forwarders $DNS \
 --foreman-proxy-dns-reverse $DNS_REV \
---foreman-proxy-dns-listen-on both
+--foreman-proxy-dns-listen-on both \
 --foreman-proxy-bmc-listen-on both \
 --foreman-proxy-logs-listen-on both \
 --foreman-proxy-realm-listen-on both \
